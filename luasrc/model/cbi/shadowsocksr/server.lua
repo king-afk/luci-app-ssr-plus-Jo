afd475db4d83c3f98831f3ab5cd173a873824df7
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-12-28 08:13:39 +0800

    Update server.lua

--- luasrc/model/cbi/shadowsocksr/server.lua
+++ luasrc/model/cbi/shadowsocksr/server.lua
@@ -77,7 +77,7 @@ sec.anonymous = true
 sec.addremove = true
 sec.sortable =  true
 sec.template = "cbi/tblsection"
-sec.extedit = luci.dispatcher.build_url("admin/services/shadowsocksr/server/%s")
+sec.extedit = luci.dispatcher.build_url("admin/vpn/shadowsocksr/server/%s")
 function sec.create(...)
 	local sid = TypedSection.create(...)
 	if sid then

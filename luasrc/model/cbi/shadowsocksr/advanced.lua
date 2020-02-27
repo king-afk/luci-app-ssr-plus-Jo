3e646909441deba7b725970b23fb29c6eee92728
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-27 23:00:26 +0800

    Update advanced.lua

--- luasrc/model/cbi/shadowsocksr/advanced.lua
+++ luasrc/model/cbi/shadowsocksr/advanced.lua
@@ -1,61 +1,119 @@
 local shadowsocksr = "shadowsocksr"
 local uci = luci.model.uci.cursor()
 local server_table = {}
+local encrypt_methods = {
+	"none",
+	"table",
+	"rc4",
+	"rc4-md5-6",
+	"rc4-md5",
+	"aes-128-cfb",
+	"aes-192-cfb",
+	"aes-256-cfb",
+	"aes-128-ctr",
+	"aes-192-ctr",
+	"aes-256-ctr",	
+	"bf-cfb",
+	"camellia-128-cfb",
+	"camellia-192-cfb",
+	"camellia-256-cfb",
+	"cast5-cfb",
+	"des-cfb",
+	"idea-cfb",
+	"rc2-cfb",
+	"seed-cfb",
+	"salsa20",
+	"chacha20",
+	"chacha20-ietf",
+}
 
-local gfwmode=0
-local gfw_count=0
-local ip_count=0
+local protocol = {
+	"origin",
+	"verify_deflate",		
+	"auth_sha1_v4",
+	"auth_aes128_sha1",
+	"auth_aes128_md5",
+	"auth_chain_a",
+	"auth_chain_b",
+	"auth_chain_c",
+	"auth_chain_d",
+	"auth_chain_e",
+	"auth_chain_f",
+}
 
-if nixio.fs.access("/etc/dnsmasq.ssr/gfw_list.conf") then
-gfwmode=1		
-end
+obfs = {
+	"plain",
+	"http_simple",
+	"http_post",
+	"random_head",	
+	"tls1.2_ticket_auth",
+}
 
-local sys = require "luci.sys"
+local raw_mode = {
+	"faketcp",
+	"udp",
+	"icmp",
+}
 
-if gfwmode==1 then 
- gfw_count = tonumber(sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l"))/2
+local seq_mode = {
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+}
 
-end
- 
-if nixio.fs.access("/etc/china_ssr.txt") then 
- ip_count = sys.exec("cat /etc/china_ssr.txt | wc -l")
-end
+local cipher_mode = {
+	"none",
+	"xor",
+	"aes128cbc",
+}
 
-uci:foreach(shadowsocksr , "servers", function(s)
-	if s["type"] == "v2ray" then
-		if s.alias then
-			server_table[s[".name"]] = "[%s]:%s" %{string.upper(s.type), s.alias}
-		elseif s.server and s.server_port then
-			server_table[s[".name"]] = "[%s]:%s:%s" %{string.upper(s.type), s.server, s.server_port}
-		end
+local auth_mode = {
+	"none",
+	"simple",
+	"md5",
+	"crc32",
+}
+
+local speeder_mode = {
+	"0",
+	"1",
+}
+
+uci:foreach(shadowsocksr, "servers", function(s)
+	if s.alias then
+		server_table[s[".name"]] = "[%s]:%s" %{string.upper(s.type), s.alias}
+	elseif s.server and s.server_port then
+		server_table[s[".name"]] = "[%s]:%s:%s" %{string.upper(s.type), s.server, s.server_port}
 	end
 end)
 
 local key_table = {}   
-for key,_ in pairs(server_table) do 
-	
-	table.insert(key_table,key)  
-	
+for key,_ in pairs(server_table) do  
+    table.insert(key_table,key)  
 end 
 
 table.sort(key_table)
+
 m = Map(shadowsocksr)
 
--- [[ 服务器节点故障自动切换设置 ]]--
-
+-- [[ global ]]--
 s = m:section(TypedSection, "global", translate("Server failsafe auto swith settings"))
 s.anonymous = true
 
 o = s:option(Flag, "monitor_enable", translate("Enable Process Deamon"))
 o.rmempty = false
+o.default = "1"
 
 o = s:option(Flag, "enable_switch", translate("Enable Auto Switch"))
 o.rmempty = false
+o.default = "1"
 
 o = s:option(Value, "switch_time", translate("Switch check cycly(second)"))
 o.datatype = "uinteger"
 o:depends("enable_switch", "1")
-o.default = 3600
+o.default = 667
 
 o = s:option(Value, "switch_timeout", translate("Check timout(second)"))
 o.datatype = "uinteger"
@@ -66,6 +124,8 @@ o = s:option(Value, "switch_try_count", translate("Check Try Count"))
 o.datatype = "uinteger"
 o:depends("enable_switch", "1")
 o.default = 3
+
+
 
 -- [[ SOCKS5 Proxy ]]--
 if nixio.fs.access("/usr/bin/ssr-local") then
@@ -92,6 +152,8 @@ o = s:option(Value, "http_port", translate("HTTP Port"))
 o.datatype = "port"
 o.default = 1081
 o.rmempty = false
+
+end
 end
 -- [[ adblock ]]--
 s = m:section(TypedSection, "global", translate("adblock settings"))
@@ -102,7 +164,6 @@ o.rmempty = false
 
 o = s:option(Value, "adblock_url", translate("adblock_url"))
 o.default = "https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt"
-
 
 -- [[ haProxy ]]--
 
@@ -123,7 +184,9 @@ o.default = "admin"
 o = s:option(Value, "admin_password", translate("Password"))
 o.default = "root"
 
-end
+
+
+
 return m
 
 
